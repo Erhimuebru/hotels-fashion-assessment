e8811acdf29d7040c392dc06a2002caa83bfe36b
erhimu ebrue <erhimuebru87@gmail.com>
2023-07-29 17:05:27 +0100

    search box

--- src/components/Search/Search.jsx
+++ src/components/Search/Search.jsx
@@ -16,14 +16,14 @@ function SearchComponents() {
         type="text"
         placeholder="Find a space" />
     </div>
-    <div className="sear flex flex-row space-x-2 w-96 gap-10 lg:gap-32 sm:gap-14 bg-white p-3">
+    <div className="sear flex flex-row space-x-2 w-96 gap-6 lg:gap-32 sm:gap-14 bg-white p-3">
       <input
         className="input durations bg-white border-none outline-none h-9 max-w-screen-lg border-r-8"
         type="text"
 
         placeholder="Duration" />
 
-      <RiArrowDropDownLine className='arrow h-6 w-10 text-[gray]' />
+      <RiArrowDropDownLine className='arrow h-8 w-12 text-[gray]' />
     </div>
 
     <div className='search-icons bg-red-950 ml-2'>
