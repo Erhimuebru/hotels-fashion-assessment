e4682833f29ef7f4462f9ca6cf0b67677ee13911
erhimu ebrue <erhimuebru87@gmail.com>
2023-07-29 16:34:29 +0100

    search box

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -4,51 +4,30 @@ import Banner from "../../assets/hotel.jpeg";
 import { BiSearch } from 'react-icons/bi';
 import { RiArrowDropDownLine } from 'react-icons/ri';
 import "./Hero.css";
+import SearchComponents from "../Search/Search";
+
 
 const Hero = () => {
   return (
   
-    <div className="mt-28 relative">
-       <img
-         style={{ filter: "brightness(50%)" }}
-         className="hero-image"
-         src={Banner}
-         alt="Banner"
-      />
-    <div className="absolute inset-0  justify-center items-center text-center text-white">
-    <div className="hero-content">
-         <h1 className="hero-title">FIND A HOME FOR YOUR DESIGNS</h1>
-         <p className="hero-subtitle">Pop Ups in Top Class Hotels Around The World</p>
-    
-    </div>
-             <div className="search flex flex-row justify-center items-end mt-96">
-             <div className="flex flex-row space-x-2 w-96 bg-white p-3">
-             <BiSearch className='h-8 pt-2 w-8 text-[gray]' />
-        <input
-          className="input bg-white border-none outline-none h-9 max-w-screen-lg border-r-8"
-          type="text"
-          placeholder="Find a space"
-        />
-          </div>
-          <div className="sear flex flex-row space-x-2 w-96 gap-32 bg-white p-3">
-              <input
-          className="input durations bg-white border-none outline-none h-9 max-w-screen-lg border-r-8"
-          type="text"
-          
-          placeholder="Duration"
-        />
-    
-          <RiArrowDropDownLine className='h-9 w-12 text-[gray]' />
-              </div>
+    <><div className="mt-28 relative">
+      <img
+        style={{ filter: "brightness(50%)" }}
+        className="hero-image"
+        src={Banner}
+        alt="Banner" />
+      <div className="absolute inset-0  justify-center items-center text-center text-white">
+        <div className="hero-content">
+          <h1 className="hero-title">FIND A HOME FOR YOUR DESIGNS</h1>
+          <p className="hero-subtitle">Pop Ups in Top Class Hotels Around The World</p>
 
-              <div className='search-icons bg-red-950 ml-2'>
-        <BiSearch className="search-btn h-6 w-6 text-white" />
+        </div>
+       <SearchComponents/>
       </div>
-    
-              </div> 
-    </div>
 
-  </div>
+    </div>
+  
+    </>
   );
 }
 
--- src/components/Search/Search.jsx
+++ src/components/Search/Search.jsx
@@ -1,7 +1,4 @@
-import React, { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
-import { AdjustmentsVerticalIcon } from "@heroicons/react/24/solid";
-import { XCircleIcon, MapPinIcon } from "@heroicons/react/24/solid";
+import React from 'react';
 import { BiSearch } from 'react-icons/bi';
 import { RiArrowDropDownLine } from 'react-icons/ri';
 import "./Search.css"
@@ -11,33 +8,29 @@ function SearchComponents() {
  
 
   return (
-    <div className="flex flex-row justify-center items-center pb-2">
-      <div className="flex flex-row space-x-2 w-96 bg-white p-3">
-        <BiSearch className='h-8 pt-2 w-8 text-[gray]' />
-        <input
-          className="bg-white border-none outline-none h-9 max-w-screen-lg border-r-8"
-          type="text"
-          placeholder="Find a space"
-        />
-      </div>
-<br />
+    <div className="search flex flex-row justify-center items-end mt-96">
+    <div className="sear flex flex-row space-x-2 w-96 bg-white p-3">
+      <BiSearch className='h-8 pt-2 w-8 text-[gray]' />
+      <input
+        className="input bg-white border-none outline-none h-9 max-w-screen-lg border-r-8"
+        type="text"
+        placeholder="Find a space" />
+    </div>
+    <div className="sear flex flex-row space-x-2 w-96 gap-32 bg-white p-3">
+      <input
+        className="input durations bg-white border-none outline-none h-9 max-w-screen-lg border-r-8"
+        type="text"
 
-      <div className=" flex flex-row space-x-2 w-96 gap-32 bg-white p-3">
-      
-        <input
-          className="durations bg-white border-none outline-none h-9 max-w-screen-lg border-r-8"
-          type="text"
-          
-          placeholder="Duration"
-        />
-    
-          <RiArrowDropDownLine className='h-9 w-12 text-[gray]' />
-      </div>
-      <div className='bg-red-950 p-5 ml-2'>
-        <BiSearch className="h-6 w-6 text-white" />
-      </div>
-    
- </div>
+        placeholder="Duration" />
+
+      <RiArrowDropDownLine className='h-6 w-10 text-black' />
+    </div>
+
+    <div className='search-icons bg-red-950 ml-2'>
+      <BiSearch className="search-btn h-6 w-6 text-white" />
+    </div>
+
+  </div>
 );
 }
 
