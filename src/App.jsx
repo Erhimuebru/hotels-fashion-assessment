7bb4b77575cef8a8d7cdd9a11a041d2172e323a0
erhimu ebrue <erhimuebru87@gmail.com>
2023-07-30 00:19:33 +0100

    Loading component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,25 @@
 import React from "react";
+import { useState, useEffect } from "react";
 import './App.css'
 import { Routes, Route } from "react-router-dom";
 import HomePage from "./pages/Home/Homepage";
 import Navbar from "./components/Navbar/Navbar";
+import Loading from "./components/Loading/Loading";
+
 function App() {
+  const [isLoading, setIsLoading] = useState(true);
 
+useEffect(() => {
+    const timeout = setTimeout(() => {
+      setIsLoading(false);
+    }, 3000);
 
+    return () => clearTimeout(timeout);
+  }, []);
+
+  if (isLoading) {
+    return <div><Loading/></div>;
+  }
   return (
     <>
     <Navbar/>
